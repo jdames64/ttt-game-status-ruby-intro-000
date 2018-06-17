011c42f9a29582c05bff39e827bff57a4bd3db5c
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 01:19:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,5 @@ def won?(board)
     position_1 = board[win_index_1] # load the value of the board at win_index_1
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1) #detect returns first element (position_1) & make sure position is taken (that it's either an X or O).
-  end
+end
 end
