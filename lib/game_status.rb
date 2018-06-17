c97af2d9a87d7cd08f91c9f8e916607d96307a06
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 03:12:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,11 +16,29 @@ WIN_COMBINATIONS = [
   [2,4,6],
 ]
 
-def won?(board)
+ddef won?(board)
 
-  WIN_COMBINATIONS.detect do |win_array|
-      win_array.all?{|win_position| board[win_position] == "X"} || win_array.all?{|win_position| board[win_position] == "O"}
+  if
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    elsif position_1 == "X" && position_2 == "O" && position_3 == "O"
+      return false
+    else
+    false
     end
+  end
+end
 end
 
 def full?(board)
