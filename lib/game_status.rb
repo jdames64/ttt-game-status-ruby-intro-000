d1c2225f3b5cc1281541db3e76e1d82c625f7cda
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 01:52:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,7 +37,7 @@ def won?(board)
       return win_combination
     elsif position_1 == "X" && position_2 == "O" && position_3 == "X"
       return false
-    else position_1 == " " && position_2 == " " && position_3 == " "
+    else position_1 == nil && position_2 == nil && position_3 == nil
     end
   end
 end
